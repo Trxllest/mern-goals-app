5927abf84045ce46128720a03fd42c14495651de
David <damilear@outlook.com>
2022-08-18 21:50:48 -0400

    Added delete goal

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -41,16 +41,14 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
     // CHECK FOR USER
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
     
     // Make sure logged in user matches the goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -72,16 +70,15 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal not found')
     }
     
-    const user = await User.findById(req.user.id)
 
     // CHECK FOR USER
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
     
     // Make sure logged in user matches the goal user
-    if(goal.user.toString() !== user.id) {
+    if(goal.user.toString() !== req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
